30885acd222f63161371a1dc9c6022ef79cba0bf
Shawn Turple <sturple@sturple.local>
2021-04-23 15:22:21 -0700

    first commit

---
+++ wordpress-marketplace.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package WordPressMarketPlace
+ */
+/*
+Plugin Name: WordPress MarketPlace
+Plugin URI: https://akismet.com/
+Description:  WordPress MarketPlace is Used to create a Unique MarketPlace for Plugins and Themes
+Version: 1.0
+Author: Turple
+Author URI: https://example.com
+License: GPLv2 or later
+Text Domain: wpmarket-place
+*/
+
